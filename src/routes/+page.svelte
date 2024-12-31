ac7192dbe9488ae79a0e87815e61e1001b2f9533
CyzmiX <itzmedigamingx@gmail.com>
2024-12-31 22:31:46 +0100

    created toast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,8 @@ let loading = $state(false);
       return;
     } else {
       created = true
-      setTimeout(() => created = '', 2000);
+      setTimeout(() => created = false, 2000);
+      return;
     }
 
   }
@@ -232,8 +233,8 @@ const debounce = (fn, delay) => {
                 aria-label="Close"
                 class="ml-2 text-white hover:text-gray-300 transition-colors "
                 onclick={() => {
-                  created = '';
-                  setTimeout(() => created = '', 2000);
+                  created = false;
+                  setTimeout(() => created = false, 2000);
                 }}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
